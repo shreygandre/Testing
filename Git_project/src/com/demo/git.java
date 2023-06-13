b82f0b5d4b8e3261cf84e1d6dd62596a7cff4699
shreygandre <136463692+shreygandre@users.noreply.github.com>
2023-06-13 18:23:36 +0530

    Update git.java

--- Git_project/src/com/demo/git.java
+++ Git_project/src/com/demo/git.java
@@ -7,7 +7,6 @@ public static void main(String []args)
 	System.out.println("hello");
 	System.out.println("hello shreyas");
 	System.out.println("Hello harshit");
-	System.out.println("hello harshit");
 }
 
 }
